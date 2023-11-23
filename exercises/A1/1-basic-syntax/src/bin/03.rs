88f4bcc9f58b8cf3bd237e7a764e665180be33ba
Dion Dokter <diondokter@gmail.com>
2023-11-23 11:14:55 +0100

    Clarify excercise 3

--- exercises/A1/1-basic-syntax/src/bin/03.rs
+++ exercises/A1/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
-    // TODO
+    // TODO, loop through the numbers and keep track of the largest and smallest values
 
     println!("{} is largest and {} is smallest");
 }
