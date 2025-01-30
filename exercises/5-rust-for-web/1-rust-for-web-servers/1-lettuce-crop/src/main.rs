4b887d412cc1b2d9141eeec940e547eab7bf76d0
Michiel <michielp1807@gmail.com>
2025-01-30 10:27:06 +0100

    Use IPv6 by default

--- exercises/5-rust-for-web/1-rust-for-web-servers/1-lettuce-crop/src/main.rs
+++ exercises/5-rust-for-web/1-rust-for-web-servers/1-lettuce-crop/src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     let app = Router::new().route("/", get("Hello, world!"));
 
     // Serve the website on localhost
-    let addr = "0.0.0.0:7000";
+    let addr = "[::]:7000";
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("Serving on http://{addr}");
     axum::serve(listener, app).await.unwrap();
